7f887b93e1870aff66b832e5d13ff126f0f81157
DCH <136453649+dcooper-holmes@users.noreply.github.com>
2023-10-05 14:36:02 +0100

    100-Days-Of-Code

---
+++ Personal_Projects/Days-In-Month/Days-In-Month.py
@@ -0,0 +1,25 @@
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        print("Leap year.")
+      else:
+        print("Not leap year.")
+    else:
+      return True
+  else:
+    return False
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+
+    if month == 2 and is_leap(year):
+      month_days[month - 1] = 29
+      return month_days[month - 1]
+    else:
+      return month_days[month - 1]
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
