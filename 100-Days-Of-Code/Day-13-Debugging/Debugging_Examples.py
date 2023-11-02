c8cc272ed14f04690c5bb5359e8e95c32cf4fb54
dcooper-holmes <136453649+dcooper-holmes@users.noreply.github.com>
2023-11-02 18:16:11 +0000

    100-Days-Of-Code

--- 100-Days-Of-Code/Day-13-Debugging/Debugging_Examples.py
+++ 100-Days-Of-Code/Day-13-Debugging/Debugging_Examples.py
@@ -2,7 +2,7 @@
 
 # # Describe Problem
 # def my_function():
-#   for i in range(1, 21):
+#   for i in range(1, 20):
 #     if i == 20:
 #       print("You got it")
 # my_function()
@@ -10,7 +10,7 @@
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 5)
+# dice_num = randint(1, 6)
 # print(dice_imgs[dice_num])
 
 # # Play Computer
