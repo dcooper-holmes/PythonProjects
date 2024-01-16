5c6254a9104213cedc4d0285f6d7a0e6d3d89167
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-16 15:22:08 +0000

    Add User Input For User Bet Variable

--- 100-Days-Of-Code/Day-19-Turtle-Race/Main.py
+++ 100-Days-Of-Code/Day-19-Turtle-Race/Main.py
@@ -4,6 +4,8 @@ tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
 
+user_bet = screen.textinput("Make your bet!", "Which turtle will win the race? Enter a color: ")
+print(user_bet)
 
 
 screen.exitonclick()
