2cf7bd9291f30f05d6652f8a6f747ce1fbfd80da
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-16 15:40:39 +0000

    Create For Loop To Create Turtles

--- 100-Days-Of-Code/Day-19-Turtle-Race/Main.py
+++ 100-Days-Of-Code/Day-19-Turtle-Race/Main.py
@@ -1,11 +1,19 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
 screen = Screen()
 screen.setup(width=500, height=400)
 
 user_bet = screen.textinput("Make your bet!", "Which turtle will win the race? Enter a color: ")
-print(user_bet)
+
+y_positions = [-70, -40, -10, 20, 50, 80]
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
 
 
 screen.exitonclick()
