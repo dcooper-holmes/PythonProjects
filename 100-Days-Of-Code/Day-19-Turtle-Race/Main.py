5615b05703eda44eef3c953e659b3fd62b1daaee
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-16 15:54:14 +0000

    Check whether turtle is at end and end game

--- 100-Days-Of-Code/Day-19-Turtle-Race/Main.py
+++ 100-Days-Of-Code/Day-19-Turtle-Race/Main.py
@@ -27,6 +27,18 @@ if user_bet:
 while is_race_on:
 
     for turtle in all_turtles:
+
+        if turtle.xcor() > 230:
+
+            is_race_on = False
+
+            winning_color = turtle.pencolor()
+            
+            if winning_color == user_bet:
+                print(f"You have Won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You have Lost! The {winning_color} turtle is the winner!")
+
         random_distance = random.randint(0, 10)
         turtle.forward(random_distance)
 
