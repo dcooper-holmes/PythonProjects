fcf03c30fa46ac6c90594428781c3dd9a481ee43
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-16 15:11:46 +0000

    Create Etcher Sketch Example

--- 100-Days-Of-Code/Day-19-Turtle-Race/Main.py
+++ 100-Days-Of-Code/Day-19-Turtle-Race/Main.py
@@ -4,10 +4,28 @@ tim = Turtle()
 screen = Screen()
 
 def move_forwards():
-    tim.forward(1)
+    tim.forward(50)
 
+def move_backwards():
+    tim.backward(50)
 
+def turn_left():
+    tim.left(10)
+
+def turn_right():
+    tim.right(10)
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+    
 screen.listen()
-screen.onkey(fun=move_forwards, key="space")
+screen.onkey(fun=move_forwards, key="w")
+screen.onkey(fun=move_backwards, key="s")
+screen.onkey(fun=turn_left, key="a")
+screen.onkey(fun=turn_right, key="d")
+screen.onkey(fun=clear_screen, key="c")
 
 screen.exitonclick()
