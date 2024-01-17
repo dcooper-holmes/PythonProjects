cfb942c3f6c0800bcc67115ed68fbdd5d2add758
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-17 13:34:01 +0000

    Create segment movement for loop

--- 100-Days-Of-Code/Day-20-Snake-Game/main.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/main.py
@@ -26,4 +26,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
+    for segment_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[segment_num - 1].xcor()
+        new_y = segments[segment_num - 1].ycor()
+        segments[segment_num].goto(new_x, new_y)
+
 screen.exitonclick()
