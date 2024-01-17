a0048b2198bf8f97da8c8f53cda228253893e271
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-17 13:09:59 +0000

    Add 3 Snake Segments

--- 100-Days-Of-Code/Day-20-Snake-Game/main.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/main.py
@@ -5,19 +5,16 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
 
+segment_1 = Turtle("square")
+segment_1.color("white")
 
+segment_2 = Turtle("square")
+segment_2.color("white")
+segment_2.goto(-20, 0)
 
-
-
-
-
-
-
-
-
-
-
-
+segment_3 = Turtle("square")
+segment_3.color("white")
+segment_3.goto(-40, 0)
 
 
 screen.exitonclick()
