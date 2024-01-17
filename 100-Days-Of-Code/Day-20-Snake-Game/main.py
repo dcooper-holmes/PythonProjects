ca4abfff11d7e6792e76822f9c7ef6bedd330941
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-17 13:42:55 +0000

    Move snake code to snake.py to create snake class

--- 100-Days-Of-Code/Day-20-Snake-Game/main.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/main.py
@@ -7,28 +7,10 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in starting_positions:
-
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-
-    segments.append(new_segment)
-
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    for segment_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[segment_num - 1].xcor()
-        new_y = segments[segment_num - 1].ycor()
-        segments[segment_num].goto(new_x, new_y)
 
 screen.exitonclick()
--- 100-Days-Of-Code/Day-20-Snake-Game/snake.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/snake.py
@@ -1,2 +1,26 @@
 from turtle import Turtle
 import time
+
+class Snake:
+
+    def __init__(self):
+        
+        starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+        segments = []
+
+        for position in starting_positions:
+
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+
+            segments.append(new_segment)
+
+    def move(self):
+            
+        for segment_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[segment_num - 1].xcor()
+            new_y = self.segments[segment_num - 1].ycor()
+            self.segments[segment_num].goto(new_x, new_y)
