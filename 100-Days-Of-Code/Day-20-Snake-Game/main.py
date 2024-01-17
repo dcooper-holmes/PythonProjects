4435e6bc5aa410052e3f68799fd494a5b95bd561
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-17 13:52:26 +0000

    Update snake class

--- 100-Days-Of-Code/Day-20-Snake-Game/main.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,10 +8,15 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
+snake = Snake()
+
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    
+    snake.move
+
 
 screen.exitonclick()
--- 100-Days-Of-Code/Day-20-Snake-Game/snake.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/snake.py
@@ -4,7 +4,6 @@ import time
 class Snake:
 
     def __init__(self):
-        
         starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
         segments = []
@@ -18,8 +17,8 @@ class Snake:
 
             segments.append(new_segment)
 
-    def move(self):
-            
+    def move(self):        
+                
         for segment_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[segment_num - 1].xcor()
             new_y = self.segments[segment_num - 1].ycor()
