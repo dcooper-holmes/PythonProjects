9cc3062b89538809cda756069067cfe34835c6d8
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-17 13:12:34 +0000

    Create For Loop To Simplify Segment Creation

--- 100-Days-Of-Code/Day-20-Snake-Game/main.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/main.py
@@ -5,16 +5,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
 
-segment_1 = Turtle("square")
-segment_1.color("white")
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
+for position in starting_positions:
 
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(-40, 0)
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 
 screen.exitonclick()
