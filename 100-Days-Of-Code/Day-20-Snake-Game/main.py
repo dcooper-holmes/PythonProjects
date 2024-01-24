c8c3c2af569784ba973483366a39af0d52277aaf
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-24 15:57:25 +0000

    Update main.py

--- 100-Days-Of-Code/Day-20-Snake-Game/main.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/main.py
@@ -12,20 +12,18 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 
+screen.listen()
+screen.onkey(fun=snake.up, key="Up")
+screen.onkey(fun=snake.down, key="Down")
+screen.onkey(fun=snake.left, key="Left")
+screen.onkey(fun=snake.right, key="Right")
+
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    
     snake.move()
-
-    screen.onkey(fun=snake.up, key="Up")
-    screen.onkey(fun=snake.down, key="Down")
-    screen.onkey(fun=snake.left, key="Left")
-    screen.onkey(fun=snake.right, key="Right")
-
-    screen.listen()
 
 
 screen.exitonclick()
