7a0a8717ef89dd704c589881f80dc92dcf919068
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-24 16:09:20 +0000

    Add ScoreBoard functionality

---
+++ 100-Days-Of-Code/Day-20-Snake-Game/scoreboard.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+class ScoreBoard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        
+        score = 0
+        self.write("Score = " + score, True, align="center")
+
+    def UpdateScore(self):
+        score += 1
+        self.write("Score = " + score, True, align="center")
