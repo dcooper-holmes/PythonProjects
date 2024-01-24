c990452761a6029fa88993ba88aaae511e736893
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-24 16:10:50 +0000

    Update scoreboard

--- 100-Days-Of-Code/Day-20-Snake-Game/scoreboard.py
+++ 100-Days-Of-Code/Day-20-Snake-Game/scoreboard.py
@@ -6,8 +6,8 @@ class ScoreBoard(Turtle):
         super().__init__()
         
         score = 0
-        self.write("Score = " + score, True, align="center")
+        self.write(f"Score = {score}", True, align="center")
 
     def UpdateScore(self):
         score += 1
-        self.write("Score = " + score, True, align="center")
+        self.write(f"Score = {score}", True, align="center")
