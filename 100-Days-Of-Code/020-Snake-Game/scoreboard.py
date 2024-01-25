e030832f21d927d9d1193ad0652f91592a8283c4
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-01-25 08:23:19 +0000

    Create update score function

--- 100-Days-Of-Code/020-Snake-Game/scoreboard.py
+++ 100-Days-Of-Code/020-Snake-Game/scoreboard.py
@@ -1,16 +1,18 @@
 from turtle import Turtle
 
-SCORE = 0
-
 class ScoreBoard(Turtle):
+
+    score = 0
     
     def __init__(self):
         super().__init__()
-        
-        score = SCORE
-        
         self.color("white")
         self.penup()
         self.ht()
         self.goto(0, 280)
-        self.write(f"Score = {score}", True, align="center")
+        self.write(f"Score = {self.score}", False, align="center")
+
+    def UpdateScore(self):
+        self.clear()
+        self.score += 1
+        self.write(f"Score = {self.score}", False, align="center")
