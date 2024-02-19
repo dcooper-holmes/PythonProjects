79b2e7ed4b7dffc72dc2606bcc6998e5ebbf9bfe
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-02-19 10:56:33 +0000

    Add Update Text Function

--- 100-Days-Of-Code/020-Snake-Game/scoreboard.py
+++ 100-Days-Of-Code/020-Snake-Game/scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 20, "normal")
 
 class ScoreBoard(Turtle):
 
@@ -10,9 +12,13 @@ class ScoreBoard(Turtle):
         self.penup()
         self.ht()
         self.goto(0, 260)
-        self.write(f"Score = {self.score}", False, align="center", font=("Arial", 20, "normal"))
+        self.Update_ScoreBoard()
+        
+
+    def Update_ScoreBoard(self):
+        self.write(f"Score = {self.score}", False, align=ALIGNMENT, font=FONT)
 
     def UpdateScore(self):
-        self.clear()
         self.score += 1
-        self.write(f"Score = {self.score}", False, align="center", font=("Arial", 20, "normal"))
+        self.clear()
+        self.Update_ScoreBoard()
