bee1167fde01574cce1cf03bbbd1e88f5917bc8b
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-02-19 11:19:04 +0000

    Add Segment Creation and Body Collision Detection

--- 100-Days-Of-Code/020-Snake-Game/snake.py
+++ 100-Days-Of-Code/020-Snake-Game/snake.py
@@ -18,13 +18,21 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
+            self.add_segment(position)
+            
 
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
+    def add_segment(self, position):
+        
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
 
-            self.segments.append(new_segment)
+        self.segments.append(new_segment)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):        
                 
