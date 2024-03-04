c445b2552cea9e930c862ff09451d3a55ee09591
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 20:57:48 +0000

    Create Detection For Left Paddle Miss

--- 100-Days-Of-Code/022-Pong-Game/main.py
+++ 100-Days-Of-Code/022-Pong-Game/main.py
@@ -31,7 +31,16 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         ball.bounce_y()
 
+    #Detect collision with paddles
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+
+    #Detect if right paddle misses ball
+    if ball.xcor() > 380 :
+        ball.reset_position()
+
+    # Detect if right paddle misses ball
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
