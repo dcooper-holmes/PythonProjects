c04687828ab7c948907d97472207f392bbeccb4b
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 15:44:40 +0000

    Remove Player file

--- 100-Days-Of-Code/022-Pong-Game/main.py
+++ 100-Days-Of-Code/022-Pong-Game/main.py
@@ -1,8 +1,13 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from player import Player
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+
+player1 = Player()
+
+
 
 screen.exitonclick()
