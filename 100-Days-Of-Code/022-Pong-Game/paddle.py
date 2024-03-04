57bc576303358b74ea8e120d63663e5303cda0c0
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 16:11:35 +0000

    Update Paddle Class

--- 100-Days-Of-Code/022-Pong-Game/paddle.py
+++ 100-Days-Of-Code/022-Pong-Game/paddle.py
@@ -2,10 +2,10 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
-        Paddle.shape("square")
-        Paddle.color("white")
-        Paddle.shapesize(stretch_wid=5, stretch_len=1)
-        Paddle.penup()
-        Paddle.goto(350, 0)
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
