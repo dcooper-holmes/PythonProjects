5b9ddae8c02bc80a88862460a6e688754a2db011
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 15:59:55 +0000

    Move Paddle Code to paddle.py

--- 100-Days-Of-Code/022-Pong-Game/main.py
+++ 100-Days-Of-Code/022-Pong-Game/main.py
@@ -6,21 +6,6 @@ screen.setup(width=800, height=600)
 screen.tracer(0)
 screen.title("Pong")
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
 screen.listen()
 screen.onkeypress(go_up, "Up")
 screen.onkeypress(go_down, "Down")
