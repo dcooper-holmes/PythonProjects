17a9e9a380dc405400914fe72e8c6a402e179cd3
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 16:15:32 +0000

    Add Up Down Functions To Both Paddles

--- 100-Days-Of-Code/022-Pong-Game/main.py
+++ 100-Days-Of-Code/022-Pong-Game/main.py
@@ -11,8 +11,10 @@ r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
 screen.listen()
-# screen.onkeypress(go_up, "Up")
-# screen.onkeypress(go_down, "Down")
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 
