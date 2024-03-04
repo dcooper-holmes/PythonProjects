e70a5afcc0c974a3ccbe9acce2d4efdaa4968a69
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 16:05:26 +0000

    Create Paddle Class

--- 100-Days-Of-Code/022-Pong-Game/paddle.py
+++ 100-Days-Of-Code/022-Pong-Game/paddle.py
@@ -1,16 +1,11 @@
 from turtle import Turtle
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
+class Paddle(Turtle):
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+    def __init__(self):
+        super().__init__()
+        Paddle.shape("square")
+        Paddle.color("white")
+        Paddle.shapesize(stretch_wid=5, stretch_len=1)
+        Paddle.penup()
+        Paddle.goto(350, 0)
