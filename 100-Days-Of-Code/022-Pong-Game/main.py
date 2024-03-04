132fa4ccc9fdf0a25263850cb8c20eafcaa45cb3
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 16:11:24 +0000

    Create Left and Right Paddle

--- 100-Days-Of-Code/022-Pong-Game/main.py
+++ 100-Days-Of-Code/022-Pong-Game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,9 +7,12 @@ screen.setup(width=800, height=600)
 screen.tracer(0)
 screen.title("Pong")
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
 screen.listen()
-screen.onkeypress(go_up, "Up")
-screen.onkeypress(go_down, "Down")
+# screen.onkeypress(go_up, "Up")
+# screen.onkeypress(go_down, "Down")
 
 game_is_on = True
 
