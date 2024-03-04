0cf6e387fca1df5513fdd6ff82397b5ca940b72b
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 15:55:22 +0000

    Create Paddle Movement

--- 100-Days-Of-Code/022-Pong-Game/main.py
+++ 100-Days-Of-Code/022-Pong-Game/main.py
@@ -12,5 +12,13 @@ paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.penup()
 paddle.goto(350, 0)
 
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
 
 screen.exitonclick()
