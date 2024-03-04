2b4f7ed231c9a1f99e75e130b96e2cc2d81d19f2
Daniel Cooper-Holmes <dcooperholmes@noreply.git.com>
2024-03-04 20:35:59 +0000

    Create Ball Move Functionality

--- 100-Days-Of-Code/022-Pong-Game/ball.py
+++ 100-Days-Of-Code/022-Pong-Game/ball.py
@@ -7,3 +7,9 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+
+    def move(self):
+
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
