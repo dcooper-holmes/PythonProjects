32c746349327e57c2c93325c0a980a09ce2d6edb
DCooperHolmes-MacOs <136453649+dcooper-holmes@users.noreply.github.com>
2025-01-21 12:15:54 +0000

    Update task create function in main.py

--- Personal_Projects/To-do List Tracker/main.py
+++ Personal_Projects/To-do List Tracker/main.py
@@ -1,3 +1,4 @@
+import os
 from functions import *
 
 def main():
@@ -8,6 +9,8 @@ def main():
 
     while running:
         
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
         print("[Please select from the following options]")
         print("1 = View Tasks | 2 = Add Task | 3 = Remove Task | 4 = Quit")
         
@@ -17,7 +20,18 @@ def main():
             view_tasks(tasks)
 
         elif selection == "2":
-            tasks.append(create_task())
+
+            creating_tasks = True
+
+            while creating_tasks:
+                os.system('cls' if os.name == 'nt' else 'clear')
+                
+                tasks.append(create_task())
+
+                if input("Would you like to create another task? Y/N: ").lower() == "y":
+                    creating_tasks = True
+                else:
+                    creating_tasks = False
 
         elif selection == "3":
             print("Option 3 selected")
