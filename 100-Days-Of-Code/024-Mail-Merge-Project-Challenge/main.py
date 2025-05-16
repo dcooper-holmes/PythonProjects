9bc201102369dc44abc6f2d7268298ec0bfeab8c
Daniel Cooper-Holmes <dan.cooper.holmes@gmail.com>
2025-05-16 14:57:25 +0100

    Add code to automate letter creation

--- 100-Days-Of-Code/024-Mail-Merge-Project-Challenge/main.py
+++ 100-Days-Of-Code/024-Mail-Merge-Project-Challenge/main.py
@@ -8,3 +8,15 @@
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 
+invited_names = open("./Input/Names/invited_names.txt", mode="r")
+starting_letter = open("./Input/Letters/starting_letter.txt", mode="r")
+
+names = invited_names.readlines()
+letter_content = starting_letter.read()
+
+for name in names:
+
+    new_letter_content = letter_content.replace("[name]", name.strip())
+
+    with open(f"./Output/ReadyToSend/letter_for_{name.strip()}.txt", "x") as new_letter:
+        new_letter.write(new_letter_content)
