c8939131f9662aa017e2b383bfc9304953ee19dc
Daniel Cooper-Holmes <dan.cooper.holmes@gmail.com>
2025-05-19 17:22:28 +0100

    Update main.py

--- 100-Days-Of-Code/025-Working-With-CSV-Data/main.py
+++ 100-Days-Of-Code/025-Working-With-CSV-Data/main.py
@@ -14,4 +14,22 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+temp_list = data["temp"].to_list()
+#print(temp_list)
+
+# Pandas has a lot of useful functions. We can get things such as the average and the highest from the Data Frame.
+#print(data["temp"].mean())
+#(data["temp"].max())
+
+# You can also target specific data by using the .'column header/title': data.day
+#print(data.day)
+
+# You can also get specific data for specific row:
+#print(data[data.day == "Monday"])
+#print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == "Monday"]
+monday_temp_c = monday.temp
+monday_temp_f = monday.temp * 9/5 + 32
+print(monday_temp_c)
+print(monday_temp_f)
