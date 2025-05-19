57849c890825f371d231ab3ec1e52e8a5f0c2047
Daniel Cooper-Holmes <dan.cooper.holmes@gmail.com>
2025-05-19 18:08:54 +0100

    Update main.py with Squirrel Fur Colour Challenge

--- 100-Days-Of-Code/025-Working-With-CSV-Data/main.py
+++ 100-Days-Of-Code/025-Working-With-CSV-Data/main.py
@@ -11,25 +11,44 @@
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
+# import pandas
+
+# data = pandas.read_csv("weather_data.csv")
+# temp_list = data["temp"].to_list()
+# #print(temp_list)
+#
+# # Pandas has a lot of useful functions. We can get things such as the average and the highest from the Data Frame.
+# #print(data["temp"].mean())
+# #(data["temp"].max())
+#
+# # You can also target specific data by using the .'column header/title': data.day
+# #print(data.day)
+#
+# # You can also get specific data for specific row:
+# #print(data[data.day == "Monday"])
+# #print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == "Monday"]
+# monday_temp_c = monday.temp
+# monday_temp_f = monday.temp * 9/5 + 32
+# print(monday_temp_c)
+# print(monday_temp_f)
+
+#                   --Shorter Squirrel Data CSV Challenge--
+#TODO: Create a new CSV using Pandas which counts how many 'Grey', 'Cinnamon' and 'Black' squirrels there are.
+
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-temp_list = data["temp"].to_list()
-#print(temp_list)
+data = pandas.read_csv("squirrel_data.csv")
 
-# Pandas has a lot of useful functions. We can get things such as the average and the highest from the Data Frame.
-#print(data["temp"].mean())
-#(data["temp"].max())
+num_grey_squirrel = len(data[data["Primary Fur Color"] == "Gray"])
+num_cinnamon_squirrel = len(data[data["Primary Fur Color"] == "Cinnamon"])
+num_black_squirrel = len(data[data["Primary Fur Color"] == "Black"])
 
-# You can also target specific data by using the .'column header/title': data.day
-#print(data.day)
+squirrel_dict = {
+    "Fur Color": ["Grey", "Cinnamon", "Black"],
+    "Count": [num_grey_squirrel, num_cinnamon_squirrel, num_black_squirrel]
+}
 
-# You can also get specific data for specific row:
-#print(data[data.day == "Monday"])
-#print(data[data.temp == data.temp.max()])
-
-monday = data[data.day == "Monday"]
-monday_temp_c = monday.temp
-monday_temp_f = monday.temp * 9/5 + 32
-print(monday_temp_c)
-print(monday_temp_f)
+df = pandas.DataFrame(squirrel_dict)
+df.to_csv('./New_Squirrel_Fur_Data.csv')
